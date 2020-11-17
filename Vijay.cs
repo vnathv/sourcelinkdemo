fef2adc931b9c1c1b0211583f7bfba674ff0ae67
Vijay Viswanathan <vijay.viswanathan@amadeus.com>
2020-11-17 18:21:47 +0100

    first commit

---
+++ Vijay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sourcelink.Demo
+{
+    public class Vijay
+    {
+        public string FullName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+    }
+}
